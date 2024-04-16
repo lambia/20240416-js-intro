817fa1ef812af42e11f46fa6a864ed8b17d06763
Luca Lambda <luca@lambia.it>
2024-04-16 13:46:53 +0200

    minor

--- main.js
+++ main.js
@@ -1,53 +1,60 @@
 // document.writeln("Hello JS World");
 
-const a = "21"; 
-const b = 2;
 
-const risultato = a	* b;
-// risultato = 0; //riassegnazione a zero
 
-const msg = "Il risultato è: ";
 
-// document.getElementById("titolo").innerHTML = "Hello External JS World"
-// document.getElementById("titolo").innerHTML = a + b;
-
-const ilMioID = "titolo";
-document.getElementById( ilMioID ).innerHTML = "La tua password è: " + nuovaPassword;
-
-// alert("Messaggio invasivo");
-
-// dichiarazione variabile
+// Dichiaro (con let) una variabile, senza assegnare valore 
 let miaStringa;
-
-let userAuthorized = false;
-userAuthorized = true;
-
-// assegnazione
-miaStringa = 'ciao';
-// utilizzo/richiamo
+// Assegno il valore a una variabile già esistente (senza let)
+miaStringa = 'Ciao';
+// utilizzo/richiamo (senza let)
 alert(miaStringa);
 
-// assegnazione
 
-miaStringa = 'dihep ha detto \"ciao\" a tutti\' noi';
-// utilizzo/richiamo
-alert(miaStringa);
+
+
+//Creo una variabile e la ri-assegno più volte
+let variabileCheVaria = "Ciao";
+variabileCheVaria = "NuovoValore";
+variabileCheVaria = "Benvenuto";
+alert(variabileCheVaria); //stampo: benvenuto
+
+
+
+
+//Scrivo del testo nel DOM della pagina
+document.getElementById("titolo").innerHTML = "Hello External JS World"
+
+
+
+
+//Eseguo una moltiplicazione
+const a = 21; 
+const b = 2;
+const risultato = a	* b;
+
+//Riscrivo il testo nell'H1, sostituendolo con il risultato
+document.getElementById("titolo").innerHTML = risultato;
+
+
+
+//Nota le differenze
+miaStringa = 'Una stringa può contenere anche degli \'apici\' o "virgolette" al suo interno.';
+miaStringa = "Una stringa può contenere anche degli 'apici' o \"virgolette\" al suo interno.";
+
 
 
 /*
 Esempio di hoisting
-alert(x);
+alert(x); //x=undefined, ma nessun errore
 var x = "ciao";
 alert(x);
 */
 
-
 /*
+Naming conventions:
 camelCaseConAltreParole
-
 PascalCaseConAltreParole
-
 snake_case_con_altre_parole
-
-kebab-case-con-altre-parole
+kebab-case-con-altre-parole (ok per CSS ma non per JS)
 */
