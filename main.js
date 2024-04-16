202c94f8039ea94175e0084f515b1a0e69f1249d
Luca Lambda <luca@lambia.it>
2024-04-16 13:40:19 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,53 @@
+// document.writeln("Hello JS World");
+
+const a = "21"; 
+const b = 2;
+
+const risultato = a	* b;
+// risultato = 0; //riassegnazione a zero
+
+const msg = "Il risultato è: ";
+
+// document.getElementById("titolo").innerHTML = "Hello External JS World"
+// document.getElementById("titolo").innerHTML = a + b;
+
+const ilMioID = "titolo";
+document.getElementById( ilMioID ).innerHTML = "La tua password è: " + nuovaPassword;
+
+// alert("Messaggio invasivo");
+
+// dichiarazione variabile
+let miaStringa;
+
+let userAuthorized = false;
+userAuthorized = true;
+
+// assegnazione
+miaStringa = 'ciao';
+// utilizzo/richiamo
+alert(miaStringa);
+
+// assegnazione
+
+miaStringa = 'dihep ha detto \"ciao\" a tutti\' noi';
+// utilizzo/richiamo
+alert(miaStringa);
+
+
+/*
+Esempio di hoisting
+alert(x);
+var x = "ciao";
+alert(x);
+*/
+
+
+/*
+camelCaseConAltreParole
+
+PascalCaseConAltreParole
+
+snake_case_con_altre_parole
+
+kebab-case-con-altre-parole
+*/
