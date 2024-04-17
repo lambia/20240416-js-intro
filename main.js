4d01bf6bd35d5f50ecdc89199d34ada3d45b3f51
Luca Lambda <luca@lambia.it>
2024-04-17 12:26:54 +0200

    correzione pwd gen

--- main.js
+++ main.js
@@ -1,63 +1,15 @@
-// document.writeln("Hello JS World");
+const firstName = prompt("Inserisci il tuo nome");
+// alert( "Il tuo nome è: " + firstName );
+console.log( `Il tuo nome è: ${firstName}` );
 
+const surname = prompt("Inserisci il tuo cognome");
+console.log( `Il tuo cognome è: ${surname}` );
 
+const color = prompt("Inserisci il tuo colore preferito");
+console.log( `Il tuo colore preferito è: ${color}` );
 
+// let newPassword = "" + firstName + surname + color + "#23";
+const newPassword = `${firstName}${surname}${color}#23`;
+console.log( `La nuova password sarà: ${newPassword}` );
 
-// Dichiaro (con let) una variabile, senza assegnare valore 
-let miaStringa;
-// Assegno il valore a una variabile già esistente (senza let)
-miaStringa = 'Ciao';
-// utilizzo/richiamo (senza let)
-alert(miaStringa);
-
-
-
-
-//Creo una variabile e la ri-assegno più volte
-let variabileCheVaria = "Ciao";
-variabileCheVaria = "NuovoValore";
-variabileCheVaria = "Benvenuto";
-alert(variabileCheVaria); //stampo: benvenuto
-
-
-
-
-//Scrivo del testo nel DOM della pagina
-document.getElementById("titolo").innerHTML = "Hello External JS World"
-
-
-
-
-//Eseguo una moltiplicazione
-const a = 21; 
-const b = 2;
-const risultato = a	* b;
-
-//Riscrivo il testo nell'H1, sostituendolo con il risultato
-// document.getElementById("titolo").innerHTML = risultato;
-
-//Riscrivo il testo nell'H1, sostituendolo con il risultato e un messaggino (uso backtick)
-document.getElementById("titolo").innerHTML = `Il risultato di ${a}x${b} è: ${risultato}`;
-
-
-
-//Nota le differenze
-miaStringa = 'Una stringa può contenere anche degli \'apici\' o "virgolette" al suo interno.';
-miaStringa = "Una stringa può contenere anche degli 'apici' o \"virgolette\" al suo interno.";
-
-
-
-/*
-Esempio di hoisting
-alert(x); //x=undefined, ma nessun errore
-var x = "ciao";
-alert(x);
-*/
-
-/*
-Naming conventions:
-camelCaseConAltreParole
-PascalCaseConAltreParole
-snake_case_con_altre_parole
-kebab-case-con-altre-parole (ok per CSS ma non per JS)
-*/
+document.getElementById("password").innerHTML = newPassword;
